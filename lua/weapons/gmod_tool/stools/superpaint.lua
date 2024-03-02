bf135447fd2822e0ecbec6a938e0f266062670c4
GIGABABAIT <zhhristov@abv.bg>
2024-03-02 13:55:44 +0200

    ploop

--- lua/weapons/gmod_tool/stools/superpaint.lua
+++ lua/weapons/gmod_tool/stools/superpaint.lua
@@ -25,20 +25,21 @@ function TOOL:LeftClick( trace )
         if not IsValid(self.TargetEntity) then self:GetOwner():SendLua('notification.AddLegacy("No target entity!", NOTIFY_ERROR, 3)') return end
 
         local maxpaints = GetConVar("superpaint_maxpaints"):GetInt()
-        if self.TargetEntity.superpaints and #self.TargetEntity.superpaints >= maxpaints then 
+        if not self:GetOwner():IsSuperAdmin() and self.TargetEntity.superpaints and #self.TargetEntity.superpaints >= maxpaints then 
             self:GetOwner():SendLua( 'notification.AddLegacy("Cannot paint more decals on this prop!", NOTIFY_ERROR, 3) EmitSound("superpaintfart.mp3", vector_origin, -2, CHAN_STATIC)' )
             return
         end
 
-
-        DoSuperPaint(self.TargetEntity, self:GetClientInfo("mat"), trace.HitPos, trace.HitNormal, self:GetClientNumber("width"), self:GetClientNumber("height"))
-        EmitSound("superpaintsploink.mp3", trace.HitPos, 0, CHAN_STATIC, 1, 100)
+        local w, h = self:GetClientNumber("width"), self:GetClientNumber("height")
+        DoSuperPaint(self.TargetEntity, self:GetClientInfo("mat"), trace.HitPos, trace.HitNormal, w, h)
+        EmitSound("superpaintsploink.mp3", trace.HitPos, 0, CHAN_STATIC, 1, 100, SND_NOFLAGS, math.Remap(w, 0.01, 128, 150, 70))
     end
 end
 
 function TOOL:RightClick( trace )
     if CLIENT then return end
     if trace.HitSky or not IsValid(trace.Entity) then self:GetOwner():SendLua('notification.AddLegacy("Tried to select invalid target entity!", NOTIFY_ERROR, 3)') return end
+    if not self:GetOwner():IsSuperAdmin() and trace.Entity:GetOwner() ~= self:GetOwner() then return self:GetOwner():SendLua('notification.AddLegacy("You do not own this entity!", NOTIFY_ERROR, 3)') end
     self.TargetEntity = trace.Entity
     self:GetOwner():SendLua("LocalPlayer():GetTool('superpaint').TargetEntity = Entity("..self.TargetEntity:EntIndex()..")")
     self:ResetGhostEnt()
@@ -61,6 +62,8 @@ function TOOL:DrawHUD()
             cam.End3D()
         else
             self.GhostEnt = ClientsideModel(self.TargetEntity:GetModel())
+            self.GhostEnt:SetMaterial(self.TargetEntity:GetMaterial())
+            self.GhostEnt:SetModelScale(self.TargetEntity:GetModelScale())
             self.GhostEnt:SetNoDraw(true)
         end
     else
@@ -122,13 +125,22 @@ if CLIENT then
     language.Add("tool.superpaint.reload_use", "Unselect target entity")
 
     function TOOL.BuildCPanel( panel )
+        panel:Help("Options")
 		panel:TextEntry( "#tool.superpaint.mat", "superpaint_mat" )
 		panel:NumSlider( "#tool.superpaint.width", "superpaint_width", 0.01, 128, 2 )
         panel:NumSlider( "#tool.superpaint.height", "superpaint_height", 0.01, 128, 2 )
+        local swap = panel:Button("Swap Width and Height")
+        function swap:DoClick()
+            local oldw, oldh = GetConVar("superpaint_width"):GetFloat(), GetConVar("superpaint_height"):GetFloat()
+            RunConsoleCommand("superpaint_width", oldh)
+            RunConsoleCommand("superpaint_height", oldw)
+        end
 
-        panel:ControlHelp("Material Lists - Lists for unmounted games will not show")
+        panel:Help("Material Lists")
+        panel:ControlHelp("Lists for unmounted games will not show")
 
         local matSelecter
+        local filter
         local matLists = {generic = {}}
 
         local foundMats, gameDirs = file.Find("materials/superpaintconverted/*", "GAME")
@@ -140,6 +152,10 @@ if CLIENT then
             for k, v in pairs(matLists.generic) do
                 matSelecter:AddMaterial(k, v)
             end
+
+            if filter:GetValue() ~= "" then
+                filter:OnValueChange(filter:GetValue())
+            end
         end
 
         local niceTitles = {
@@ -147,7 +163,8 @@ if CLIENT then
             ep2 = "Half-Life 2: Episode 2",
             episodic = "Half-Life 2: Episode 1",
             left4dead2 = "Left 4 Dead 2",
-            tf = "Team Fortress 2"
+            tf = "Team Fortress 2",
+            dods = "Day of Defeat: Source"
         }
 
         for _, gameDir in pairs(gameDirs) do
@@ -159,6 +176,10 @@ if CLIENT then
                 for k, v in pairs(matLists[gameDir]) do
                     matSelecter:AddMaterial(k, v)
                 end
+
+                if filter:GetValue() ~= "" then
+                    filter:OnValueChange(filter:GetValue())
+                end
             end
 
             local foundGameMats = file.Find("materials/superpaintconverted/"..gameDir.."/*.vmt", "GAME")
@@ -167,6 +188,19 @@ if CLIENT then
             for _, name in pairs(foundGameMats) do table.insert(matLists[gameDir], "superpaintconverted/"..gameDir.."/"..name) end
         end
 
+        filter = panel:TextEntry("Quick Filter")
+        filter:SetUpdateOnType( true )
         matSelecter = panel:MatSelect("superpaint_mat", matLists.generic, true, 0.25, 0.25)
+
+        filter.OnValueChange = function( s, txt )
+            for id, pnl in ipairs( matSelecter.Controls ) do
+                if ( !pnl.Value:lower():find( txt:lower(), nil, true ) ) then
+                    pnl:SetVisible( false )
+                else
+                    pnl:SetVisible( true )
+                end
+            end
+            matSelecter:InvalidateChildren()
+        end
 	end
 end
